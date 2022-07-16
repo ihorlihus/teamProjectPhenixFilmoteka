24e9068d3d6bdc2462427c999c1b29178f9ffa7e
ihorlihus <93285058+ihorlihus@users.noreply.github.com>
2022-07-16 21:39:05 +0300

    Create index.js
    Create index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@ import './js/example';
 import { fetchTrendingMovies } from './js/fetchTrendingMovies';
 import { fetchQueryMovies } from './js/fetchQueryMovies';
 import { createMovieCard } from './js/createMovieCard';
-
