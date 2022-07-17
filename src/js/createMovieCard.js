f01c142909814320b3f885d536392599b76fb7fe
Anton Kutsim <kutsimanton@gmail.com>
2022-07-17 11:40:59 +0300

    p4
    add.fetchGenres.and.changes.createMovieCard

--- src/js/createMovieCard.js
+++ src/js/createMovieCard.js
@@ -1,13 +1,44 @@
+import {  localGenres } from "./fetchGenres";
+
+
+    
 export const createMovieCard = (movies) => {
-    return movies.map(movie => 
-        `
-        <a>
-            <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}' width='394'/>
-            <p>${movie.title || movie.name}</p>
-            <p>${movie.genre_ids}</p>
-            <p>${movie.vote_average}</p>
-            <p>${movie.release_date || movie.first_air_date}</p>
-        </a>
-    `
-    ).join('');
+    return movies.map( 
+        ({
+            poster_path,
+            title,
+            name,
+            release_date,
+            first_air_date,
+            genre_ids,
+            // genres,
+            vote_average,
+            
+        }) => {
+            
+            console.log(localGenres);
+            // let genres = localGenres.map(({ name }) => name).join(', ')
+            // if (genres) {
+            //     console.log(genres);
+            //     // localGenres
+            //     //     // .filter(({ id }) => genre_ids.includes(id))
+            //     //     .map(({ name }) => name)
+            //     //     .join(', ');
+            // }
+
+            return `
+                <li>
+                    <a>
+                        <img src='https://image.tmdb.org/t/p/w500${poster_path}'/>
+                        <p>${title || name}</p>
+                        <div>
+                            <p>${genre_ids}</p>
+                            <p>${(release_date || first_air_date).slice(0, 4)}</p>
+                            <span>${vote_average}</span>
+                        </div>
+                    </a>
+                </li>
+            `
+            }).join('');
 }
+
