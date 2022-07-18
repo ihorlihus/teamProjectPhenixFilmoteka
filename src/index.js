fb05a1813baed1532136255be2d9f5199967c5a0
Mixasikus <mixasiikus@gmail.com>
2022-07-18 15:17:26 +0300

    init modal-add

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './js/example';
 import { fetchTrendingMovies } from './js/fetchTrendingMovies';
 import { fetchQueryMovies } from './js/fetchQueryMovies';
 import { createMovieCard } from './js/createMovieCard';
-import './js/fetchModal';
+import './js/modal';
 import './js/modalTeam';
