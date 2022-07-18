d9fe0a69faffa221e8b087544f94fb8bc7144b96
Yevhen Bashchak <evgeny221@gmail.com>
2022-07-18 19:25:58 +0200

    test

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -1,14 +1,14 @@
 import '../sass/test.scss';
 
-// const search = document.querySelector('.search');
 const list = document.querySelector('.film_list');
-
-// search.addEventListener('submit', queryApi);
+const modal = document.querySelector('.backdrop');
+const watchButton = document.querySelector('.js-watch');
+const queueButton = document.querySelector('.js-queue');
 queryApi();
-async function queryApi(e) {
-  // e.preventDefault();
+//  фетч -----------
+async function queryApi() {
   await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=deeaaa6f7bb1ec2c1571c017660b3676&query=dog`
+    `https://api.themoviedb.org/3/trending/all/day?api_key=deeaaa6f7bb1ec2c1571c017660b3676`
   )
     .then(response => {
       if (!response.ok) {
@@ -22,53 +22,101 @@ async function queryApi(e) {
     })
     .catch(Error => console.log(Error));
 }
-function showGallery(results) {
-  list.innerHTML = renderGallery(results);
-  const cardOfList = document.querySelectorAll('.photo-card');
-  cardOfList.addEventListener('click', modalcard);
+// показ галереї
+async function showGallery(results) {
+  list.innerHTML = await renderGallery(results);
 }
-
+// рендер галереї
 function renderGallery(results) {
   return results
-    .map(({ original_title, backdrop_path, id }, index) => {
-      return `<div class="photo-card" width="100px">
+    .map(({ original_title, backdrop_path, id }) => {
+      return `<a class="link_card" href="" data-id="${id}"><div class="photo-card" width="100px">
              <img class="photo-card-image" src="https://image.tmdb.org/t/p/w500/${backdrop_path}" alt=" " loading="lazy" />
           <div class="info">
             <p class="info-item">
                  <b>${original_title}</b>
           
-         </div></div>`;
+         </div></div></a>`;
     })
     .join('');
 }
+// Рендер модального вікна
+function createFilmCard(data) {
+  const {
+    vote_average,
+    vote_count,
+    genres,
+    original_title,
+    title,
+    poster_path,
+    original_name,
+    popularity,
+    overview,
+    id,
+  } = data;
 
-function modalcard(e) {
-  e.preventDefault();
-  console.log('modal is working');
+  return `<div class="about_film-card">
+         <img src="https://image.tmdb.org/t/p/w500${poster_path}" class="about_film-img" alt="" loading="lazy" data-id=${id} />
+        <div class="about_film-info">
+          <h1 class="about_film-name">${title}
+          </h1>
+          <div class="about_film-item">
+          <p class="lng-vote about_film-text">Vote / Votes</p>
+          <b class="about_film-date">${vote_average} / ${vote_count}</b>
+          </div>
+          <div class="about_film-item">
+          <p class="lng-popul about_film-text">Popularity</p>
+          <b class="about_film-date">${popularity}</b>
+          </div>
+          <div class="about_film-item">
+          <p class="lng-oridg about_film-text">Original Title</p>
+          <b class="about_film-date">${
+            original_title ? original_title : original_name
+          }</b>
+          </div>
+          <div class="about_film-item">
+          <p class="lng-genre about_film-text">Genre</p>
+          <b class="about_film-date">${
+            genres ? genres.map(genre => genre.name) : ' '
+          }</b>
+          </div>
+          <h2 class="lng-about about_film-pretitle">ABOUT</h2>
+          <p class="about_film-overview">${overview}</p>
+        </div>
+      </div>`;
+}
+// запит для модалки
+async function request(id) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=deeaaa6f7bb1ec2c1571c017660b3676`
+    );
+    const film = response.json();
+    return film;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-//// <a class="link_card" href="https://api.themoviedb.org/3/movie/${id}?api_key=deeaaa6f7bb1ec2c1571c017660b3676">  запит за ід
+list.addEventListener('click', e => {
+  e.preventDefault();
+  request(e.target.dataset.id)
+    .then(data => {
+      const cardObject = data;
+      modal.insertAdjacentHTML('afterbegin', createFilmCard(data));
 
-const Localstor = {
-  id: 324252,
-
-  request() {
-    fetch(
-      `href="https://api.themoviedb.org/3/movie/${this.id}?api_key=deeaaa6f7bb1ec2c1571c017660b3676"`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+      watchButton.addEventListener('click', addToWatchStorage(cardObject));
+      queueButton.addEventListener('click', addToQueueStorage(cardObject));
+    })
+    .catch(error => {
+      console.log(error);
     });
-  },
+});
 
-  // об'єкт картки фільму
-
-  // перевірка сховища  --якщо так сет-ітем.сторидж(try(); catch()), парсинг
-  // перевірка на include цього об'єкта --якщо так - кнопка змінена на "видалити"
-  // якщо ні то
-  // пуш до масиву
-  // json.stringify масиву
-  // гет.ітем
-};
+function addToWatchStorage(cardObject) {
+  console.log(cardObject);
+  localStorage.setItem('Watch', JSON.stringify(cardObject));
+}
+function addToQueueStorage(cardObject) {
+  localStorage.setItem('Queue', JSON.stringify(cardObject));
+}
