571d7b3ffe82ae32d1010bc3cad15f9f1e520996
AndreyKrykun <andreykrykun1995@gmail.com>
2022-07-21 17:36:07 +0300

    update-scroll/spinner

--- src/js/infinityScroll.js
+++ src/js/infinityScroll.js
@@ -1,88 +1,79 @@
-import { createMovieCard } from './createMovieCard';
+import {createMovieCard} from './createMovieCard';
 import { fetchQueryMovies } from './fetchQueryMovies';
 import { fetchTrendingMovies } from './fetchTrendingMovies';
-import { refs } from './refs';
+import {refs} from './refs';
 import Spinner from './spinner';
 
-let fetchOptions = {
-  currentPage: 1,
+export let fetchOptions = {
+    currentPage: 1,
 };
+
+export const resetPage = () => {
+    fetchOptions.currentPage = 1;
+}
 
 const observerOptions = {
-  rootMargin: '0px',
-  threshold: 1.0,
+    rootMargin: '-100px',
+    threshold: 1.0
 };
-
+    
 const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      onLoadMore();
-      console.log('is intersecting');
-    }
-  });
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            onLoadMore();
+        };
+    });
 }, observerOptions);
 
 setObserverOn();
 
 function setObserverOn() {
-  observer.observe(document.querySelector('.scroll-check'));
-}
-
+    observer.observe(document.querySelector('.scroll-check'));
+};
+    
 function setObserverOff() {
-  observer.unobserve(document.querySelector('.scroll-check'));
-}
+    observer.unobserve(document.querySelector('.scroll-check'));
+};
 
 const spinner = new Spinner({
-  loader: '.loader',
-  hidden: true,
-});
+    loader: '.loader',
+    hidden: true,
+})
 
 function onLoadMore() {
-  fetchTrendingMovies().then(movies => {
-    try {
-      fetchOptions.currentPage += 1;
-      refs.gallery.insertAdjacentHTML(
-        'beforeend',
-        createMovieCard(movies.results)
-      );
-      spinner.hide();
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  spinner.show();
 
+    fetchTrendingMovies().then(movies => {
+        try {
+            if(fetchOptions.currentPage === movies.total_pages) {
+                refs.gallery.insertAdjacentHTML('beforeend', createMovieCard(movies.results));
+                window.alert('Sorry this is the last page, we do not have any movies for you :(');
+                spinner.hide();
+                setObserverOff();
+                return ;
+            }
+            fetchOptions.currentPage += 1;
+            refs.gallery.insertAdjacentHTML('beforeend', createMovieCard(movies.results));
+            // spinner.hide();
+        } catch (error) {
+            console.log(error);
+        }
+    }); 
 
-    // fetchQueryMovies().then(movies => {
-    //     try {
-    //         fetchOptions.currentPage += 1;
-    //         refs.gallery.insertAdjacentHTML('beforeend', createMovieCard(movies.results));
-    //         spinner.hide();     
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // });
+    fetchQueryMovies().then(movies => {
+        try {
+            if(fetchOptions.currentPage === movies.total_pages) {
+                refs.gallery.insertAdjacentHTML('beforeend', createMovieCard(movies.results));
+                window.alert('Sorry this is the last page, we do not have any movies for you :(');
+                spinner.hide();
+                setObserverOff();
+                return;
+            }
+            fetchOptions.currentPage += 1;
+            refs.gallery.insertAdjacentHTML('beforeend', createMovieCard(movies.results));
+            // spinner.hide();     
+        } catch (error) {
+            console.log(error);
+        }
+    });
 }
-    
-    
-
-
-
-// function drawMovies(data) {
-//     const movies = data.hits;
-//     totalMovies = data.totalHits;
-
-//     if (!totalMovies) {
-//         Notify.failure('Sorry, there are NO MOVIES matching your search query. Please try again.');
-
-//         refs.form.reset();
-//         return;
-//     };
-
-//     if (fetchOptions.currentPage === 1) {
-//         Notify.success(`Wow! We found for you${totalMovies} movies.`);
-//     };
-
-//     createMovieCard(movies.results);
-
-//     setObserverOn();
-// };
