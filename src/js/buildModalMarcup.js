f456aaa378414bd7458268bd0a7ae787f3eb6559
ihorlihus <93285058+ihorlihus@users.noreply.github.com>
2022-07-22 12:12:32 +0300

    Revert "сделал для таблетки и декстопа модалку"

--- src/js/buildModalMarcup.js
+++ src/js/buildModalMarcup.js
@@ -12,29 +12,26 @@ export function openModalMovie(data) {
     id,
   } = data;
 
-  const popularityToString = popularity.toString();
-
-  return `<img class="img-modal" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" data-id=${id} />
+  return `
+        <img class="img-modal" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" data-id=${id} />
         <div class="container-modal">
           <h1 class="title-modal">${title}</h1>
           <table class="table">
-            <tr class="table-separator">
+            <tr>
               <th class="table__text">Vote / Votes</th>
               <th><span class="vote-average">${vote_average}</span> / <span class="vote-count">${vote_count}</span></th>
             </tr>
-            <tr class="table-separator">
+            <tr>
               <th class="table__text">Popularity</th>
-              <th>${popularityToString.slice(0, 5)}</th>
+              <th>${popularity}</th>
             </tr>
-            <tr class="table-separator">
+            <tr>
               <th class="table__text">Original Title</th>
               <th>${original_title ? original_title : original_name}</th>
             </tr>
-            <tr class="table-separator">
+            <tr>
               <th class="table__text">Genre</th>
-              <th>${
-                genres ? genres.map(genre => genre.name).slice(0, 2) : ' '
-              }</th>
+              <th>${genres ? genres.map(genre => genre.name) : ' '}</th>
             </tr>
           </table>
           <h2 class="title-about__modal">ABOUT</h2>
