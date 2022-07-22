e61bb717b57bf6251cab35d7f85e494120fc1032
Mixasikus <mixasiikus@gmail.com>
2022-07-22 12:55:20 +0300

    k

--- src/js/buildModalMarcup.js
+++ src/js/buildModalMarcup.js
@@ -12,26 +12,29 @@ export function openModalMovie(data) {
     id,
   } = data;
 
-  return `
-        <img class="img-modal" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" data-id=${id} />
+  const popularityToString = popularity.toString();
+
+  return `<img class="img-modal" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" data-id=${id} />
         <div class="container-modal">
           <h1 class="title-modal">${title}</h1>
           <table class="table">
-            <tr>
+            <tr class="table-separator">
               <th class="table__text">Vote / Votes</th>
               <th><span class="vote-average">${vote_average}</span> / <span class="vote-count">${vote_count}</span></th>
             </tr>
-            <tr>
+            <tr class="table-separator">
               <th class="table__text">Popularity</th>
-              <th>${popularity}</th>
+              <th>${popularityToString.slice(0, 5)}</th>
             </tr>
-            <tr>
+            <tr class="table-separator">
               <th class="table__text">Original Title</th>
               <th>${original_title ? original_title : original_name}</th>
             </tr>
-            <tr>
+            <tr class="table-separator">
               <th class="table__text">Genre</th>
-              <th>${genres ? genres.map(genre => genre.name) : ' '}</th>
+              <th>${
+                genres ? genres.map(genre => genre.name).slice(0, 2) : ' '
+              }</th>
             </tr>
           </table>
           <h2 class="title-about__modal">ABOUT</h2>
