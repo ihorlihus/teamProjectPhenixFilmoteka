3fda59cf4f1c1d3dd18810eadd91ce4009da1d1a
Anton Kutsim <kutsimanton@gmail.com>
2022-07-22 20:50:21 +0300

    p.16
    add.if.to.fetch.genres

--- src/js/fetchGenres.js
+++ src/js/fetchGenres.js
@@ -9,23 +9,23 @@ export const fetchGenres = async () => {
     }
 }
 
-export let localGenres;
-
-
+// function onGetGenres() {
+//     if (localStorage.getItem('genres')) {
+//         console.log("GENRES");
+//         return;
+//     } else {
+//         fetchGenres().then(genres => {
+//             console.log("NO GNR");
+//             localStorage.setItem("genres", JSON.stringify(genres.genres));
+//         })
+//     }
+// }
+// onGetGenres();
 
 fetchGenres().then(genres => {
-    localStorage.setItem("genres", JSON.stringify(genres.genres));
-    const saveGenres = localStorage.getItem("genres");
-    localGenres = JSON.parse(saveGenres);
-});
-
-// if (localStorage.getItem("genres")) {
-//     return;
-// } else {
-//     fetchGenres().then(genres => {
-//         localStorage.setItem("genres", JSON.stringify(genres.genres));
-//         const saveGenres = localStorage.getItem("genres");
-//         localGenres = JSON.parse(saveGenres);
-//     });
-    
-// }
+    if ("genres") {
+        return;
+    } else {
+        localStorage.setItem("genres", JSON.stringify(genres.genres));
+    }
+})
