e7bd5c31630ab4c3ef27143b997d6eba66ba8fdd
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-07-23 14:51:28 +0200

    Second version, correct js code, changed color, add animation

--- src/js/modalTeamVersion.js
+++ src/js/modalTeamVersion.js
@@ -1,5 +1,5 @@
 
-  const refs = {
+const refs = {
     openModalBtn: document.querySelector('.backdrop-modal'),
     closeModalBtn: document.querySelector('.modal-btn'),
     modal: document.getElementById('modalTeam'),
@@ -12,15 +12,14 @@
 function onOpenModal(event) {
    event.preventDefoult();
    refs.openModalBtn.classList.toggle('is-hidden-modal');
+   
+  }
+
+function onCloseModal() {
+  refs.openModalBtn.classList.toggle('is-hidden-modal');
   window.addEventListener('keydown', onEscKeyPress);
-    console.log('open')
-  }
 
-  function onCloseModal() {
-    refs.openModalBtn.classList.toggle('is-hidden-modal');
-    console.log('close')
-  }
-
+}
 
   function onEscKeyPress(event) {
     if (event.code === 'Escape') {
