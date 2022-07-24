ad1484c889ce12941b38344c8904536eba56b831
Anton Kutsim <kutsimanton@gmail.com>
2022-07-24 22:59:56 +0300

    Update fetchTrendingMovies.js

--- src/js/fetchTrendingMovies.js
+++ src/js/fetchTrendingMovies.js
@@ -7,7 +7,6 @@ export const fetchTrendingMovies = async (spinner) => {
       `https://api.themoviedb.org/3/trending/movie/week?api_key=ff98b74c6ada2972698b8eff6707845a&page=${fetchOptions.currentPage}`
 
     );
-  console.log(response);
   spinner.hide()
   return response.json();
 
@@ -17,4 +16,4 @@ export const fetchTrendingMovies = async (spinner) => {
 };
 
 
-fetchTrendingMovies().then(data => console.log(data));
+// fetchTrendingMovies().then(data => console.log(data));
