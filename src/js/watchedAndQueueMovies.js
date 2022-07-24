9187828eb9ad5b93aef9cb05d7d6aca6a4850969
LiliiaNikolaichuk <l.nikolaychuk@ukr.net>
2022-07-24 18:48:49 +0300

    Update-library

--- src/js/watchedAndQueueMovies.js
+++ src/js/watchedAndQueueMovies.js
@@ -1,10 +1,12 @@
 import { refs } from './refs.js';
 import { createMovieCard } from './libraryMovieCard';
+import { renderLibrary } from './renderLibrary';
 
+renderLibrary()
 
 refs.watchedBtnLib.addEventListener('click', onWatchedBtnClick);
 refs.queueBtnLib.addEventListener('click', onQueueBtnClick);
-refs.gallery.addEventListener('click', onGalleryContainerClick);
+//refs.gallery.addEventListener('click', onGalleryContainerClick);
 
 export function onWatchedBtnClick() {
     refs.gallery.innerHTML = ''
@@ -13,22 +15,29 @@ export function onWatchedBtnClick() {
     //console.log(parsedGetWatched)
     if (parsedGetWatched.length !== 0) {
         refs.gallery.insertAdjacentHTML('afterbegin', createMovieCard(parsedGetWatched));
+        refs.fillerContainer.innerHTML = ''
+    } else {
+        refs.fillerContainer.innerHTML = `<p class="fill-text">Your list is empty. Choose a movie.</p>`;
     }
 }
 
 export function onQueueBtnClick() {
+    refs.watchedBtnLib.classList.remove('button-active');
     refs.gallery.innerHTML = ''
     let getQueue = localStorage.getItem('Queue')
     const parsedGetQueue = JSON.parse(getQueue)
     //console.log(parsedGetQueue)
     if (parsedGetQueue.length !== 0) {
         refs.gallery.insertAdjacentHTML('afterbegin', createMovieCard(parsedGetQueue));
+        refs.fillerContainer.innerHTML = ''
+    } else {
+        refs.fillerContainer.innerHTML = `<p class="fill-text">Your list is empty. Choose a movie.</p>`;
     }
 }
 
-function onGalleryContainerClick(evt) {
-    evt.preventDefault()
-    if (evt.target.nodeName !== 'IMG') {
-        return;
-    }
-}
+//function onGalleryContainerClick(evt) {
+    //evt.preventDefault()
+    //if (evt.target.nodeName !== 'IMG') {
+        //return;
+    //}
+//}
