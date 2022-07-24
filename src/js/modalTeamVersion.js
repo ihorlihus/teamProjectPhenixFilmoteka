3343b3a188c77d99548dc28d4d328657dc0f31f9
ihorlihus <ligusigor@ukr.net>
2022-07-24 15:43:43 +0300

    Інша модалка, превент дефолт

--- src/js/modalTeamVersion.js
+++ src/js/modalTeamVersion.js
@@ -1,32 +1,24 @@
-
 const refs = {
-    openModalBtn: document.querySelector('.backdrop-modal'),
-    closeModalBtn: document.querySelector('.modal-btn'),
-    modal: document.getElementById('modalTeam'),
-  };
+  openModalBtn: document.querySelector('.backdrop-modal'),
+  closeModalBtn: document.querySelector('.modal-btn'),
+  modal: document.getElementById('modalTeam'),
+};
 
-  refs.modal.addEventListener('click', onOpenModal)
-  refs.closeModalBtn.addEventListener('click', onCloseModal);
-  
+refs.modal.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
 
 function onOpenModal(event) {
-   event.preventDefoult();
-   refs.openModalBtn.classList.toggle('is-hidden-modal');
-   
-  }
+  event.preventDefault();
+  refs.openModalBtn.classList.toggle('is-hidden-modal');
+}
 
 function onCloseModal() {
   refs.openModalBtn.classList.toggle('is-hidden-modal');
   window.addEventListener('keydown', onEscKeyPress);
-
 }
 
-  function onEscKeyPress(event) {
-    if (event.code === 'Escape') {
-      onCloseModal();
-    }
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    onCloseModal();
   }
-
-
-
-
+}
