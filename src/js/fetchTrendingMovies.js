b4c965b6315b3ae87bc608233db4bf856b0d2e1b
Yevhen Bashchak <evgeny221@gmail.com>
2022-07-26 08:12:19 +0200

    login

--- src/js/fetchTrendingMovies.js
+++ src/js/fetchTrendingMovies.js
@@ -2,12 +2,8 @@ import { fetchOptions } from './infinityScroll';
 
 export const fetchTrendingMovies = async spinner => {
   try {
-<<<<<<< HEAD
-    spinner.show();
-=======
     console.log(fetchOptions.currentPage);
-    spinner.show()
->>>>>>> c59cb9028f1430a1767328a457f4fa93b7215dd9
+    spinner.show();
     const response = await fetch(
       `https://api.themoviedb.org/3/trending/movie/week?api_key=ff98b74c6ada2972698b8eff6707845a&page=${fetchOptions.currentPage}`
     );
@@ -18,12 +14,8 @@ export const fetchTrendingMovies = async spinner => {
   }
 };
 
-<<<<<<< HEAD
-=======
-
 // fetchTrendingMovies().then(movies => {
 //   refs.gallery.innerHTML = createMovieCard(movies.results);
 // });
->>>>>>> c59cb9028f1430a1767328a457f4fa93b7215dd9
 // fetchTrendingMovies().then(data => console.log(data));
 // ${fetchOptions.currentPage}
